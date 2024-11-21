e88ebda130fbff98a74018e09e89c2ec68508d0e
ravidu dilusha <86191475+ravindudilusha@users.noreply.github.com>
2024-11-22 00:51:19 +0530

    adding footer and animations

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -44,57 +44,77 @@ function Header() {
               </li>
             </ul>
             <ul className="navbar-nav d-flex align-items-center gap-2">
-  <li className="dropdown">
-    <Dropdown>
-      <Dropdown.Toggle variant="light" className="custom-dropdown-toggle">
-        <Flag code="US" style={{ width: 20, height: 15 }} /> EN{" "}
-      </Dropdown.Toggle>
+              <li className="dropdown">
+                <Dropdown>
+                  <Dropdown.Toggle
+                    variant="light"
+                    className="custom-dropdown-toggle"
+                  >
+                    <Flag code="US" style={{ width: 20, height: 15 }} /> EN{" "}
+                  </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1" className="custom-dropdown-item">
-          <Flag code="US" style={{ width: 20, height: 15 }} /> EN
-        </Dropdown.Item>
-        <Dropdown.Item href="#/action-2" className="custom-dropdown-item">
-          <Flag code="IN" style={{ width: 20, height: 15 }} /> Hindi
-        </Dropdown.Item>
-        <Dropdown.Item href="#/action-3" className="custom-dropdown-item">
-          <Flag code="ES" style={{ width: 20, height: 15 }} /> Español
-        </Dropdown.Item>
-        <Dropdown.Item href="#/action-4" className="custom-dropdown-item">
-          <Flag code="FR" style={{ width: 20, height: 15 }} /> Français
-        </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  </li>
-  <li className="nav-item d-flex align-middle">
-    <button type="button" className="btn">
-      Login
-    </button>
-    <button type="button" className="login-button">
-      Sign up
-    </button>
-  </li>
-</ul>
-
+                  <Dropdown.Menu>
+                    <Dropdown.Item
+                      href="#/action-1"
+                      className="custom-dropdown-item"
+                    >
+                      <Flag code="US" style={{ width: 20, height: 15 }} /> EN
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      href="#/action-2"
+                      className="custom-dropdown-item"
+                    >
+                      <Flag code="IN" style={{ width: 20, height: 15 }} /> Hindi
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      href="#/action-3"
+                      className="custom-dropdown-item"
+                    >
+                      <Flag code="ES" style={{ width: 20, height: 15 }} />{" "}
+                      Español
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      href="#/action-4"
+                      className="custom-dropdown-item"
+                    >
+                      <Flag code="FR" style={{ width: 20, height: 15 }} />{" "}
+                      Français
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+              <li className="nav-item d-flex align-middle">
+                <button type="button" className="btn">
+                  Login
+                </button>
+                <button type="button" className="login-button">
+                  Sign up
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
       <hr className="m-0 p-0"></hr>
       <div className="row">
-      <div className="col-4 container mt-3">
-      <div className="position-relative">
-        <input
-          type="text"
-          className="form-control  ps-5 search-input" // Adds padding-left for the icon
-          placeholder="Search ringtone"
-        />
-        <CiSearch
-          className="position-absolute"
-          style={{ top: '50%', left: '10px', transform: 'translateY(-50%)' }} // Centers the icon vertically
-        />
+        <div className="col-md-4 col-12 container mt-3">
+          <div className="position-relative">
+            <input
+              type="text"
+              className="form-control  ps-5 search-input" // Adds padding-left for the icon
+              placeholder="Search ringtone"
+            />
+            <CiSearch
+              className="position-absolute"
+              style={{
+                top: "50%",
+                left: "10px",
+                transform: "translateY(-50%)",
+              }} // Centers the icon vertically
+            />
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   );
 }
